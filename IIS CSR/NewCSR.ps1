996e6633f4d3596dcf09cd8020537bc0274207d6
Mvdraa <68705299+Mvdraa@users.noreply.github.com>
2024-10-14 19:56:17 +0200

    Add files via upload

---
+++ IIS CSR/NewCSR.ps1
@@ -0,0 +1,34 @@
+Invoke-Command -ScriptBlock {   
+    $CertName = Read-Host "CN van certificaat? (i.e. *.mitchellvanderaa.com)"
+    $customerName = Read-Host "Klantnaam?"
+    $Signature = 'Windows NT$'
+    $INFPath = "c:\temp\CSR_$($customerName).inf"
+    $CSRPath = "c:\temp\$($customerName).csr"
+
+    $INF = @"
+    [Version]
+    Signature= "$Signature" 
+    [NewRequest]
+    Subject = "CN=$CertName, OU=ICT, O=Novion, L=NB, S=Veldhoven, C=NL"
+    KeySpec = 1
+    KeyLength = 2048
+    Exportable = TRUE
+    MachineKeySet = TRUE
+    SMIME = False
+    PrivateKeyArchive = FALSE
+    UserProtected = FALSE
+    UseExistingKeySet = FALSE
+    ProviderName = "Microsoft RSA SChannel Cryptographic Provider"
+    ProviderType = 12
+    RequestType = PKCS10
+    KeyUsage = 0xa0
+    [EnhancedKeyUsageExtension]
+    OID=1.3.6.1.5.5.7.3.1
+"@
+
+    Write-Host "CSR being generated...."
+    $INF | out-file -FilePath $INFPath -Force
+    certreq -new $INFPath $CSRPath 
+    Get-Content $CSRPath | Set-Clipboard
+    Write-Host "CSR Request created check ctrl+v"
+}
